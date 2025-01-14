7791dc4e078a5b74cfd70572e9f687b1c4d58671
Israel Alves <israel_alves77@hotmail.com>
2025-01-14 09:00:11 -0300

    hotfix: colocar importações

--- venda-service/src/main/java/com/sos/venda_service/VendaServiceApplication.java
+++ venda-service/src/main/java/com/sos/venda_service/VendaServiceApplication.java
@@ -1,7 +1,10 @@
 package com.sos.venda_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @RefreshScope
